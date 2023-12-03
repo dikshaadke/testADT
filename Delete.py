4478a6bb6eb80db2af386c4c6439799c1839570a
Diksha Adke <47685149+dikshaadke@users.noreply.github.com>
2023-12-02 23:28:05 -0500

    Update Delete.py

--- Delete.py
+++ Delete.py
@@ -2,9 +2,8 @@ import streamlit as st
 from pymongo import MongoClient
 import os
 
-
 # MongoDB setup
-conn_str = "mongodb+srv://imdb_adt:imdb_adt@cluster0.rcvxgzc.mongodb.net/"
+conn_str = "your_connection_string"
 client = MongoClient(conn_str, serverSelectionTimeoutMS=60000)
 db = client['Moviedatabase']
 collection = db['moviedata_vectordb']
@@ -17,9 +16,12 @@ movie_name = st.text_input("Enter the movie name:", "Parasite_2019")
 
 # Button to delete reviews
 if st.button('Delete Non-helpful Reviews'):
-    # MongoDB operation to delete reviews
-    delete_criteria = {"MovieName": movie_name, "helpful": 0}
-    result = collection.delete_many(delete_criteria)
-    
-    # Display the result
-    st.write(f"Documents deleted: {result.deleted_count}")
+    try:
+        # MongoDB operation to delete reviews
+        delete_criteria = {"MovieName": movie_name.strip(), "helpful": 0}
+        result = collection.delete_many(delete_criteria)
+        
+        # Display the result
+        st.write(f"Documents deleted: {result.deleted_count}")
+    except Exception as e:
+        st.write("An error occurred:", e)
