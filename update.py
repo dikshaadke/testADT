5063d36b66f92788e50fdf47c8e43793d02f99de
Diksha Adke <47685149+dikshaadke@users.noreply.github.com>
2023-12-03 16:40:16 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,64 +1,44 @@
-from pymongo import MongoClient
-from config import *
 import streamlit as st
-import pandas as pd
-import pymongo
-import numpy as np
+from pymongo import MongoClient
+import os
 
-st.set_page_config(layout="wide")
-st.title("Update")
-conn_str = "mongodb+srv://imdb_adt:imdb_adt@cluster0.rcvxgzc.mongodb.net/"
+# MongoDB setup
+conn_str = "mongodb+srv://imdb_adt:imdb_adt@cluster0.rcvxgzc.mongodb.net"
 client = MongoClient(conn_str, serverSelectionTimeoutMS=60000)
 db = client['Moviedatabase']
 collection = db['moviedata']
 
-revs = []
-for rev in collection.find({"MovieName":"Parasite_2019"}).sort("helpful",pymongo.DESCENDING).limit(11):
-    if rev['source']=="imdb":
-        revs.append(rev)
-df = pd.DataFrame(revs)
+# Streamlit UI
+st.title("Movie Review Manager")
 
-ids = df['_id'].tolist()
-helpful = df['helpful'].tolist()
-total_votes = df['total_votes'].tolist()
-helpful_votes = [str(int(helpful[i])) + "/" + str(int(total_votes[i])) for i in range(len(df))]
-df['helpful/votes'] = helpful_votes
-# df['found helpful'] = [False for _ in range(len(df))]
-df.drop(['_id','MovieName','date','title','link','if_spoiler','source','helpful','total_votes'],inplace=True,axis=1)
+# Input for movie name and review ID
+movie_name = st.text_input("Enter the movie name:", "")
+review_id = st.text_input("Enter the review ID to mark as helpful:", "")
 
-# st.dataframe(df,use_container_width=True,hide_index=True)
-def dataframe_with_selections(df):
-    df_with_selections = df.copy()
-    df_with_selections.insert(3, "found helpful?", False)
-    edited_df = st.data_editor(
-        df_with_selections,
-        hide_index=True,
-        column_config={"found helpful?": st.column_config.CheckboxColumn(required=True)},
-        disabled=df.columns,
-        use_container_width=True
-    )
-    selected_indices = list(np.where(edited_df['found helpful?'])[0])
-    return selected_indices
+# Button to mark review as helpful
+if st.button('Mark Review as Helpful'):
+    try:
+        # MongoDB operation to update the review
+        update_criteria = {
+            "_id": review_id,
+            "MovieName": movie_name.strip()
+        }
+        update_action = {
+            "$inc": {
+                "helpful": 1,
+                "total_votes": 1
+            }
+        }
+        result = collection.update_one(update_criteria, update_action)
+        
+        # Display the result
+        if result.matched_count > 0:
+            st.write(f"Review updated successfully. Matched Count: {result.matched_count}, Modified Count: {result.modified_count}")
+        else:
+            st.write("No matching document found.")
+    except Exception as e:
+        st.write("An error occurred:", e)
 
-
-selection = dataframe_with_selections(df)
-selected_ids = [ids[select_id] for select_id in selection]
-
-def update_database(selected_ids):
-    collection.update_many
-    for select_id in selected_ids:
-        collection.update_many({'_id': select_id}, [{'$inc': {'helpful': 1}},{'$inc': {'total_votes': 1}}])
-if 'clicked' not in st.session_state:
-    st.session_state.clicked = False
-
-def click_button():
-    st.session_state.clicked = True
-
-st.button("Submit Update",help="It will submit the reviews that you selected to the database",on_click=click_button)
-
-if st.session_state.clicked:
-    update_database(selected_ids)
-    st.write("Updated the database")
 
 
 
